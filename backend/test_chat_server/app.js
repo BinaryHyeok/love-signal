94ed1f97e2d5050643eb4b104b5fbfaca33bcb38
kevin_Arpe <arpeggiogio@gmail.com>
2023-04-22 21:04:36 +0900

    update: 클라이언트의 채팅 내용 수신 테스트

--- backend/test_chat_server/app.js
+++ backend/test_chat_server/app.js
@@ -1,17 +1,22 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Server Running");
 });
 
-http.listen(3001, () => {
-  console.log("Connected at port 3001");
+http.listen(5000, () => {
+  console.log("Connected at port 5000");
 });
 
 io.on("connection", (socket) => {
   socket.on("req_msg", (msg) => {
+    console.log("msg : " + msg);
     io.emit("res_msg", msg);
   });
 

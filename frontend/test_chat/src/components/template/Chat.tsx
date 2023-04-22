fbd63fdd4390ad48e5cb60e05b40d037c680544c
kevin_Arpe <arpeggiogio@gmail.com>
2023-04-22 21:03:54 +0900

    update: 채팅서버와 msg 관련 통신 테스트

--- frontend/test_chat/src/components/template/Chat.tsx
+++ frontend/test_chat/src/components/template/Chat.tsx
@@ -1,14 +1,42 @@
-import React, { useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { io } from "socket.io-client";
 
+const socket = io("http://localhost:5000");
+
 const Chat = () => {
+  const [msg, setMsg] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
 
+  useEffect(() => {
+    socket.on("res_msg", (msg: string) => {
+      chatListUpdateHandler(msg);
+    });
+  }, []);
+
+  const chatListUpdateHandler = (msg: string) => {
+    console.log(msg);
+  };
+
+  const chatSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const myMsg = inputRef.current != null ? inputRef.current.value : "";
+    socket.emit("req_msg", myMsg);
+  };
+
+  const chatInputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMsg(e.target.value);
+  };
+
   return (
-    <div>
-      <input type="text" ref={inputRef} />
+    <form onSubmit={chatSubmitHandler}>
+      <input
+        type="text"
+        ref={inputRef}
+        onChange={chatInputChangeHandler}
+        value={msg}
+      />
       <button>메시지 전송</button>
-    </div>
+    </form>
   );
 };
 

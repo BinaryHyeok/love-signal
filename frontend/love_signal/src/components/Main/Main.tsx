ae601e3b8f85441f831f7db262872e5d2e17131d
손종효 <jbson1998@naver.com>
2023-04-25 16:06:56 +0900

    메인추가 및 asset 폴더 추가

--- frontend/love_signal/src/components/Main/Main.tsx
+++ frontend/love_signal/src/components/Main/Main.tsx
@@ -1,7 +1,31 @@
 import style from "./Main.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
-  return <>처음 메인 화면</>;
+  const navigate = useNavigate();
+  const goToLoginPage = () => {
+    navigate("/Login");
+  };
+  const goToSignUpPage = () => {
+    navigate("/SignUp");
+  };
+  return (
+    <>
+      <div className={style.MainContainer}>
+        <div className={style.mainImg}>
+          <img src="/assets/Main.png" />
+        </div>
+        <div className={style.buttonBox}>
+          <button className={style.button} onClick={goToLoginPage}>
+            Login
+          </button>
+          <button className={style.button} onClick={goToSignUpPage}>
+            Join
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Main;
--- frontend/love_signal/src/components/Manual/Manual.tsx
+++ frontend/love_signal/src/components/Manual/Manual.tsx
@@ -2,7 +2,11 @@ import style from "./Manual.module.scss";
 
 const Manual = () => {
   return (
-    <>사용 설명서가 들어갈 페이지?입니다.(회의를 통해 모달로 뺄지 결정하기.)</>
+    <>
+      <div className={style.mainContainer}>
+        <div className={style.cellphoneModal}></div>
+      </div>
+    </>
   );
 };
 

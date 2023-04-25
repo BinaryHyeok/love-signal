422bc5ad8445b5cdb6df8bef0f77acd244b8643d
2sseul <iseul0626@gmail.com>
2023-04-25 17:59:58 +0900

    와이어프레임 수정및 추가

--- frontend/love_signal/src/components/Chat/Chat.tsx
+++ frontend/love_signal/src/components/Chat/Chat.tsx
@@ -1,7 +1,12 @@
 import style from "./Chat.module.scss";
 
 const Chat = () => {
-  return <>채팅창이 있을 공간이며 동적라우팅이 필요할 것 같은데..아마도?</>;
+  return (
+    <>
+      채팅창이 있을 공간이며 동적라우팅이 필요할 것 같은데..아마도? ㅎㅎ
+      채팅창은 많은 것이 필요합니다. 오늘도 와이어프레임 수정
+    </>
+  );
 };
 
 export default Chat;

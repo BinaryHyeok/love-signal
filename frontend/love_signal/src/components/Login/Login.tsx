66f104434c4f9057f41e526fdaeb7e4a930dda28
손종효 <jbson1998@naver.com>
2023-04-25 20:33:49 +0900

    로그인 틀 잡아놓기 Issue : S08P31B309-346

--- frontend/love_signal/src/components/Login/Login.tsx
+++ frontend/love_signal/src/components/Login/Login.tsx
@@ -1,7 +1,61 @@
 import style from "./Login.module.scss";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return <>로그인 페이지 입니다.(러프하게 진행할 예정 OAuth사용예정입니다.)</>;
+  const [myId, setMyId] = useState<string>("");
+  const [myPw, setMyPw] = useState<string>("");
+
+  const handleChangeId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    setMyId(target.value);
+  };
+
+  const handleChangePw = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    setMyPw(target.value);
+  };
+
+  const checkLogin = () => {
+    //로그인 되는지 체크해주는 함수
+    axios
+      .post("/경로어디어디뭐시기/member/auth/sign-in", {
+        id: myId,
+        pw: myPw,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <div className={style.loginTainer}>
+        <input
+          type="text"
+          id="email"
+          value={myId}
+          name="email"
+          onChange={handleChangeId}
+          className={style.loginTextform}
+          placeholder="test@test.com"
+        />
+        <input
+          type="text"
+          id="email"
+          value={myPw}
+          name="email"
+          onChange={handleChangePw}
+          className={style.loginTextform}
+          placeholder="test@test.com"
+        />
+        <button onClick={checkLogin}>로그인</button>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- frontend/love_signal/src/components/Signup/SignUp.tsx
+++ frontend/love_signal/src/components/Signup/SignUp.tsx
@@ -1,7 +1,101 @@
 import style from "./SignUp.module.scss";
+import { useState } from "react";
+import axios from "axios";
 
 const SignUp = () => {
-  return <>간단한 회원가입을 구현해놓는 페이지입니다.</>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [nickname, setNickname] = useState<string>("");
+  const [checkNickname, setCheckNickname] = useState<boolean>(false);
+
+  const handleChangeId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    setEmail(target.value);
+  };
+
+  const handleChangePw = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    setPassword(target.value);
+  };
+
+  const handleChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    setNickname(target.value);
+    setCheckNickname(false); //바뀌면 바로 false로 바꿔줘.
+  };
+
+  //중복확인 해주는 함수입니다.
+  const duplecheck = () => {
+    axios
+      .get(`https://어쩌구저쩌구/member/nickname/check/${nickname}`)
+      .then((response) => {
+        //여기로 와서 만약 response가 중복되지 않은것을 알려주었다면?
+        if (response) {
+          setCheckNickname(true); //중복확인 체크하기.
+        }
+      })
+      .catch((err) => {
+        console.log(err); //에러발생!
+      });
+  };
+
+  //회원가입 버튼 클릭했을때
+  const signup = () => {
+    if (checkNickname) {
+      //중복 확인 했을때만 가능하다
+      axios
+        .post("https://어쩌구저쩌구주소다/member/auth/sign-up", {
+          email: email,
+          password: password,
+          nickname: nickname,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      //아니면 중복확인 눌러줘~
+      alert("중복확인 체크는 하셨나요?");
+    }
+  };
+
+  return (
+    <>
+      <div className={style.loginTainer}>
+        <input
+          type="text"
+          id="email"
+          value={nickname}
+          name="email"
+          onChange={handleChangeNickname}
+          className={style.loginTextform}
+          placeholder="test@test.com"
+        />
+        <button onClick={duplecheck}>중복확인</button>
+        <input
+          type="text"
+          id="email"
+          value={email}
+          name="email"
+          onChange={handleChangeId}
+          className={style.loginTextform}
+          placeholder="test@test.com"
+        />
+        <input
+          type="password"
+          id="email"
+          value={password}
+          name="email"
+          onChange={handleChangePw}
+          className={style.loginTextform}
+          placeholder="test@test.com"
+        />
+      </div>
+      <button onClick={signup}>회원가입</button>
+    </>
+  );
 };
 
 export default SignUp;

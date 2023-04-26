d166bbfa499bb3b62da4fc2ff22b0dd9802ac52c
2sseul <iseul0626@gmail.com>
2023-04-26 16:51:12 +0900

    상세정보 모달

--- frontend/love_signal/src/components/UI/Modal/CheckTeam.tsx
+++ frontend/love_signal/src/components/UI/Modal/CheckTeam.tsx
@@ -1,7 +1,42 @@
 import style from "./CheckTeam.module.scss";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper";
+
 const CheckTeam = () => {
-  return <>이성팀 찾기에서 팀 구성이 완료 되지 않았을때 띄울 모달창입니다.</>;
+  return (
+    <div className={style.container}>
+      <div className={style.background}></div>
+      <div className={style.modal}>
+        <Swiper
+          pagination={{
+            type: "progressbar",
+          }}
+          loop={true}
+          // navigation={true}
+          modules={[Pagination, Navigation]}
+          className={style.swiper}
+        >
+          <SwiperSlide className={style.swiperSlide}>
+            <img src="/assets/selfieSample.jpg" />
+          </SwiperSlide>
+          <SwiperSlide className={style.swiperSlide}>
+            <img src="/assets/selfieSample2.jpg" />
+          </SwiperSlide>
+          <SwiperSlide className={style.swiperSlide}>
+            <img src="/assets/selfieSample3.jpg" />
+          </SwiperSlide>
+        </Swiper>
+        <div className={style.buttonContainer}>
+          <div>버튼</div>
+          <div>버튼</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CheckTeam;

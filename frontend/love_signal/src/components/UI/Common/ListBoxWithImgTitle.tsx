2467588af5e50e57a17974a636bc970f07c372aa
손종효 <jbson1998@naver.com>
2023-04-27 16:29:09 +0900

    이성팀 컴포넌트화

--- frontend/love_signal/src/components/UI/Common/ListBoxWithImgTitle.tsx
+++ frontend/love_signal/src/components/UI/Common/ListBoxWithImgTitle.tsx
@@ -3,7 +3,7 @@ import style from "./ListBoxWithImgTitle.module.scss";
 
 const ListBoxWithImgTitle = (props: any) => {
   return (
-    <div className={style.listBox}>
+    <div className={`${style.listBox} ${props.type}-line`}>
       <div className={style.titleBox}>{props.title}</div>
       {props.children}
     </div>

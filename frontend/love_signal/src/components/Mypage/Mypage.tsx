5ead4933c9fe419be1eeb1e12cafa68da331b5ad
손종효 <jbson1998@naver.com>
2023-04-29 15:10:47 +0900

    footer recoil추가

--- frontend/love_signal/src/components/Mypage/Mypage.tsx
+++ frontend/love_signal/src/components/Mypage/Mypage.tsx
@@ -1,11 +1,9 @@
 import Footer from "../UI/Footer/Footer";
-import Header from "../UI/Header/Header";
 import style from "./Mypage.module.scss";
 
 const Mypage = () => {
   return (
     <>
-      <Header />
       네비바를 눌렀을때 갈 마이페이지 입니다.
       <Footer />
     </>
--- frontend/love_signal/src/index.tsx
+++ frontend/love_signal/src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </>
 );
 

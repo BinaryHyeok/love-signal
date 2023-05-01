34a7f4baa34c7b3015a7ca158e4619b97a139adf
kevin-Arpe <arpeggiogio@gmail.com>
2023-05-02 00:19:40 +0900

    update: 채팅방 상대 채팅박스 디자인 & 아토믹 디자인 수정

--- frontend/love_signal/src/components/atoms/Chat/A_ChatInput.tsx
+++ frontend/love_signal/src/components/atoms/Chat/A_ChatInput.tsx
@@ -1,12 +1,22 @@
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 import style from "./styles/A_ChatInput.module.scss";
 
 type PropsTypes = {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   text: string;
+  isDisabled?: boolean;
 };
 
-const A_ChatInput: React.FC<PropsTypes> = ({ onChange, text }) => {
-  return <input className={style.chatInput} value={text} onChange={onChange} />;
+const A_ChatInput: React.FC<PropsTypes> = ({ onChange, text, isDisabled }) => {
+  return (
+    <input
+      className={`${style.chatInput} ${isDisabled ? style.disabled : ""}`}
+      value={text}
+      onChange={onChange}
+      disabled={isDisabled}
+      placeholder={isDisabled ? "채팅이 불가한 채팅방입니다." : ""}
+    />
+  );
 };
 
 export default A_ChatInput;
--- frontend/love_signal/src/components/atoms/Chat/A_ChatItemInfo.tsx
+++ frontend/love_signal/src/components/atoms/Chat/A_ChatItemInfo.tsx
@@ -5,16 +5,46 @@ type PropsType = {
   id: string;
   title: string;
   memberCount: string;
+  lastMsgTime?: string;
 };
 
-const A_ChatItemInfo: React.FC<PropsType> = ({ id, title, memberCount }) => {
+const A_ChatItemInfo: React.FC<PropsType> = ({
+  id,
+  title,
+  memberCount,
+  lastMsgTime,
+}) => {
+  const timeForMatter = (str: string) => {
+    const [s_date, s_time] = str.split(" ");
+    const [year, month, date] = s_date.split("-");
+    const [hour, min, sec] = s_time.split(":");
+
+    return {
+      year,
+      month,
+      date,
+      hour,
+      min,
+      sec,
+    };
+  };
+
+  const timeView = (sendTime: string) => {
+    const date = timeForMatter(sendTime);
+    const APM = +date.hour < 12 ? "오전" : "오후";
+
+    return `${APM} ${date.hour}:${date.min}`;
+  };
+
   return (
     <div className={style.infoBox}>
       <div className={style.titleBox}>
         <span className={style.title}>{title}</span>
         <span className={style.count}>{memberCount}</span>
       </div>
-      <div className={style.time}>오전 10:15</div>
+      <div className={style.time}>
+        {lastMsgTime ? timeView(lastMsgTime) : ""}
+      </div>
     </div>
   );
 };

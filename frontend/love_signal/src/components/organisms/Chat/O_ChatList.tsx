7dc569d32d930247ee7c2f73df3b6668da0f1948
kevin-Arpe <arpeggiogio@gmail.com>
2023-05-03 14:18:01 +0900

    update: 채팅 목록 프로필 이미지 추가

--- frontend/love_signal/src/components/organisms/Chat/O_ChatList.tsx
+++ frontend/love_signal/src/components/organisms/Chat/O_ChatList.tsx
@@ -4,32 +4,100 @@ import M_ChatItem from "../../molecules/Chat/M_ChatItem";
 import { useRecoilState } from "recoil";
 import { roomInfo } from "../../../atom/chatRoom";
 import { footerIsOn } from "../../../atom/footer";
+import { room } from "../../../types/room";
 
-const DUMMY_CHATLIST = [
+const DUMMY_CHATLIST: room[] = [
   {
     id: "1",
     title: "남자방",
     memberCount: "3",
     lastMsgTime: "2023-05-01 11:39:27",
     type: "TEAM",
+    members: [
+      {
+        nickname: "Tom",
+        age: 22,
+        description: "반가워요",
+        imgSrc:
+          "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      },
+      {
+        nickname: "Jimmy",
+        age: 26,
+        description: "Hellooooo",
+        imgSrc:
+          "https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      },
+      {
+        nickname: "Mike",
+        age: 31,
+        description: "WeoWeoWeo",
+        imgSrc:
+          "https://images.unsplash.com/photo-1590086782957-93c06ef21604?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      },
+    ],
   },
   {
     id: "2",
-    title: "미팅방",
+    title: "러브하우스",
     memberCount: "6",
     lastMsgTime: "2023-05-02 00:03:12",
     type: "GROUP",
+    members: [
+      {
+        nickname: "Tom",
+        age: 22,
+        description: "반가워요",
+        imgSrc:
+          "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      },
+      {
+        nickname: "Jimmy",
+        age: 26,
+        description: "Hellooooo",
+        imgSrc:
+          "https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      },
+      {
+        nickname: "Mike",
+        age: 31,
+        description: "WeoWeoWeo",
+        imgSrc:
+          "https://images.unsplash.com/photo-1590086782957-93c06ef21604?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      },
+      {
+        nickname: "Tom",
+        age: 22,
+        description: "반가워요",
+        imgSrc:
+          "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      },
+      {
+        nickname: "Jimmy",
+        age: 26,
+        description: "Hellooooo",
+        imgSrc:
+          "https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      },
+      {
+        nickname: "Mike",
+        age: 31,
+        description: "WeoWeoWeo",
+        imgSrc:
+          "https://images.unsplash.com/photo-1590086782957-93c06ef21604?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      },
+    ],
   },
   {
     id: "3",
-    title: "공지방",
+    title: "러브시그널",
     memberCount: "",
     lastMsgTime: "2023-05-01 23:59:59",
     type: "NOTICE",
   },
   {
     id: "4",
-    title: "익명방",
+    title: "익명",
     memberCount: "2",
     lastMsgTime: "2023-05-01 12:03:12",
     type: "ANONYMOUS",
@@ -52,14 +120,7 @@ const O_ChatList = () => {
   return (
     <ul className={style.chatList}>
       {DUMMY_CHATLIST.map((room) => (
-        <M_ChatItem
-          key={room.id}
-          id={room.id}
-          title={room.title}
-          memberCount={room.memberCount}
-          lastMsgTime={room.lastMsgTime}
-          onClick={selectRoomHandler}
-        />
+        <M_ChatItem key={room.id} room={room} onClick={selectRoomHandler} />
       ))}
     </ul>
   );

6192a36721b48bf68430594b12f9cee553e7b3e3
손종효 <jbson1998@naver.com>
2023-05-03 14:17:20 +0900

    update : Axios 데이터 타입 수정 Issue : S08P31B309-403

--- frontend/love_signal/src/types/member.ts
+++ frontend/love_signal/src/types/member.ts
@@ -9,3 +9,14 @@ export type member = {
   description: string;
   imgSrc?: string;
 };
+
+//회원정보 조회시에 받아오는 데이터(굳이 필요한가 의문)
+export type userInfo = {
+  memberUUID: string;
+  nickname: string;
+  gender: string;
+  age: number;
+  description: string;
+  teamUUID: string;
+  teamLeader: boolean;
+};

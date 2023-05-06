799190629d82292adf13be552848081b9fe826fc
kevin_Arpe <arpeggiogio@gmail.com>
2023-05-06 14:53:45 +0900

    fix: 메인 로고 해상도 높은 이미지로 교체

--- frontend/love_signal/src/components/atoms/Main/A_MainImg.tsx
+++ frontend/love_signal/src/components/atoms/Main/A_MainImg.tsx
@@ -3,7 +3,7 @@ import style from "./styles/A_MainImg.module.scss";
 const MainImg = () => {
   return (
     <div className={style.mainImg}>
-      <img src="/assets/Main.png" />
+      <img src="/assets/main_logo.png" />
     </div>
   );
 };

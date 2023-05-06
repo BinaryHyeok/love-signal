5734bf4a6debbc7149af681188c95e96256e8bc0
kevin_Arpe <arpeggiogio@gmail.com>
2023-05-06 13:12:50 +0900

    fix: unit height를 계산해 모바일에서 순수한 document 높이를 지정하도록 수정

--- frontend/love_signal/src/components/pages/Common/RootLayout.tsx
+++ frontend/love_signal/src/components/pages/Common/RootLayout.tsx
@@ -1,26 +1,28 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
-  const [viewportHeight, setViewportHeight] = useState<number>(
-    window.innerHeight
-  );
-
   useEffect(() => {
-    const handleResize = (e: UIEvent) => {
-      console.log("prev = ", viewportHeight, " next = ", window.innerHeight);
-      setViewportHeight(window.innerHeight);
-    };
-    window.addEventListener("resize", handleResize);
+    /* Scrren Resize */
+    unitHeightSetHandler();
+
+    window.addEventListener("resize", unitHeightSetHandler);
+    window.addEventListener("touchend", unitHeightSetHandler);
 
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("resize", unitHeightSetHandler);
+      window.removeEventListener("touchend", unitHeightSetHandler);
     };
   }, []);
 
+  const unitHeightSetHandler = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  };
+
   return (
     <>
-      <main className="common-bg" style={{ height: viewportHeight }}>
+      <main className="common-bg">
         <Outlet />
       </main>
     </>

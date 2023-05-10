369bf15adb2b7e7dcd5134a92164a9eeceef4cc6
kevin-Arpe <arpeggiogio@gmail.com>
2023-05-10 11:22:31 +0900

    delete: 백업용 프로젝트 삭제

--- frontend/love_signal/src/components/templates/MyTeam/T_MyTeam.tsx
+++
@@ -1,9 +0,0 @@
-type PropsType = {
-  children: any;
-};
-
-const T_MyTeam: React.FC<PropsType> = ({ children }) => {
-  return <>{children}</>;
-};
-
-export default T_MyTeam;
--- frontend/love_signal/src/types/chat.ts
+++
@@ -1,7 +0,0 @@
-export type chat = {
-  roomType?: string;
-  isMe: boolean;
-  sender?: string;
-  text: string;
-  sendTime: string;
-};

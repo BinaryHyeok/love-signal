107f1d7aa844c507ba4bcce871dfc78859655b78
BinaryHyuk <wlswl2435@naver.com>
2023-05-11 01:57:50 +0900

    [Fix] webconfig 주석

--- chatting-service/src/main/java/kr/lovesignal/chattingservice/config/WebConfig.java
+++ chatting-service/src/main/java/kr/lovesignal/chattingservice/config/WebConfig.java
@@ -8,18 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
-                        .allowedHeaders("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
-                        .exposedHeaders("Authorization", "RefreshToken");
-            }
-        };
-    }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
+//                        .allowedHeaders("*")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
+//                        .exposedHeaders("Authorization", "RefreshToken");
+//            }
+//        };
+//    }
 
 }

0d82f8eed78a6ce6479cd83fdc1ec748f42cf419
유도열 <oktui2002@naver.com>
2023-05-13 20:36:02 +0900

    [Feat] fix Serializable

--- chatting-service/src/main/java/kr/lovesignal/chattingservice/model/request/SelectOrShareInfo.java
+++ chatting-service/src/main/java/kr/lovesignal/chattingservice/model/request/SelectOrShareInfo.java
@@ -4,12 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Getter
 @Setter
 @Builder
-public class SelectOrShareInfo {
+public class SelectOrShareInfo implements Serializable {
 
     private List<String> nicknames;
     private List<String> profileUrls;

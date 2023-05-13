ef50cc3d6232645041de5eccde06900c502c6f58
유도열 <oktui2002@naver.com>
2023-05-13 18:00:10 +0900

    [Feat] fix WebSock config

--- chatting-service/src/main/java/kr/lovesignal/chattingservice/config/WebSockConfig.java
+++ chatting-service/src/main/java/kr/lovesignal/chattingservice/config/WebSockConfig.java
@@ -1,7 +1,14 @@
 package kr.lovesignal.chattingservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptorAdapter;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -21,4 +28,30 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*")
                 .withSockJS();
     }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.setInterceptors(stompChannelInterceptor());
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        registration.taskExecutor().corePoolSize(4);
+        registration.setInterceptors(stompChannelInterceptor());
+    }
+
+    @Bean
+    public ChannelInterceptorAdapter stompChannelInterceptor() {
+        return new ChannelInterceptorAdapter() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+
+                return message;
+            }
+        };
+    }
+
 }

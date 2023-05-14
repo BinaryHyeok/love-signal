f9ed44df4280d2c60f778f2f822ec86c48d4e72e
유도열 <oktui2002@naver.com>
2023-05-14 20:54:46 +0900

    [Feat] fix serializabl

--- chatting-service/src/main/java/kr/lovesignal/chattingservice/model/response/ResSelectChatRoom.java
+++ chatting-service/src/main/java/kr/lovesignal/chattingservice/model/response/ResSelectChatRoom.java
@@ -6,10 +6,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @Builder
-public class ResSelectChatRoom {
+public class ResSelectChatRoom implements Serializable {
 
     private String UUID;
     private String type;

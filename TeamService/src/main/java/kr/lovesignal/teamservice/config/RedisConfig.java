e84c6ad5379844ad1a8b263b46ed1a2d412ae6fa
BinaryHyuk <wlswl2435@naver.com>
2023-05-14 20:51:30 +0900

    [Fix] 오타 수정

--- TeamService/src/main/java/kr/lovesignal/teamservice/config/RedisConfig.java
+++ TeamService/src/main/java/kr/lovesignal/teamservice/config/RedisConfig.java
@@ -32,16 +32,6 @@ public class RedisConfig {
         return new LettuceConnectionFactory(redisConfig);
     }
 
-//    /**
-//     * redis pub/sub 메시지를 처리하는 listener 설정
-//     */
-//    @Bean
-//    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory) {
-//        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-//        container.setConnectionFactory(connectionFactory);
-//        return container;
-//    }
-
     /**
      * 어플리케이션에서 사용할 redisTemplate 설정
      */
--- TeamService/src/main/java/kr/lovesignal/teamservice/util/RedisUtils.java
+++ TeamService/src/main/java/kr/lovesignal/teamservice/util/RedisUtils.java
@@ -3,11 +3,12 @@ package kr.lovesignal.teamservice.util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class RedisUtils {
 

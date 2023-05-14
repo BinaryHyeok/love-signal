1f79355bbe0b0591f7af1b038f76e6e853828cf9
PakaOxO <arpeggiogio@gmail.com>
2023-05-14 20:36:59 +0900

    update: 조회된 이성팀이 없을 때 보여줄 컴포넌트 추가

---
+++ frontend/love-signal-pwa/src/components/molecules/OtherGender/M_NoOtherTeam.tsx
@@ -0,0 +1,7 @@
+import style from "./styles/M_NoOtherTeam.module.scss";
+
+const M_NoOtherTeam = () => {
+  return <div className={style.noTeam}>조회된 팀이 없습니다.</div>;
+};
+
+export default M_NoOtherTeam;

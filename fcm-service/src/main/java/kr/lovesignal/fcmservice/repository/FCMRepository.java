a6a5160ce1baf0bcc3b72d90fc38a016bf39001b
권오영 <kwnoyng@gmail.com>
2023-05-15 12:43:56 +0900

    Update fcm-service/src/main/java/kr/lovesignal/fcmservice/repository/FCMRepository.java

--- fcm-service/src/main/java/kr/lovesignal/fcmservice/repository/FCMRepository.java
+++ fcm-service/src/main/java/kr/lovesignal/fcmservice/repository/FCMRepository.java
@@ -3,7 +3,7 @@ package kr.lovesignal.fcmservice.repository;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.utio.optional;
+import java.util.optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 

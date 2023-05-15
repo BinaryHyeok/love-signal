602832266013faa18ad28b739777d3eabfb6abff
권오영 <kwnoyng@gmail.com>
2023-05-15 21:21:28 +0900

    Update fcm-service/src/main/java/kr/lovesignal/fcmservice/service/FCMServiceImpl.java

--- fcm-service/src/main/java/kr/lovesignal/fcmservice/service/FCMServiceImpl.java
+++ fcm-service/src/main/java/kr/lovesignal/fcmservice/service/FCMServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Isolation;
 
 
 import com.google.firebase.messaging.FirebaseMessaging;

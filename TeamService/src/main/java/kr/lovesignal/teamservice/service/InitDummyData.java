50156906c8a4da8707175d3ea4e1d4739759f021
이진혁 <wlswl2435@naver.com>
2023-05-15 12:51:42 +0900

    [feat] context path 추가

--- TeamService/src/main/java/kr/lovesignal/teamservice/service/InitDummyData.java
+++ TeamService/src/main/java/kr/lovesignal/teamservice/service/InitDummyData.java
@@ -25,7 +25,7 @@ public class InitDummyData {
     private final MeetingRepository meetingRepository;
 
     @Transactional
-//    @PostConstruct
+    @PostConstruct
     public void InitDummyData(){
 
         // 팀 생성
@@ -51,6 +51,7 @@ public class InitDummyData {
             String formattedDate = randomDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 
             MemberEntity member = MemberEntity.builder()
+                    .kakaoId(Integer.toString(idx))
                     .email(Integer.toString(idx))
                     .birth(formattedDate)
                     .nickname(Integer.toString(idx))
@@ -75,6 +76,7 @@ public class InitDummyData {
             String formattedDate = randomDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 
             MemberEntity member = MemberEntity.builder()
+                    .kakaoId(Integer.toString(idx))
                     .email(Integer.toString(idx))
                     .birth(formattedDate)
                     .nickname(Integer.toString(idx))
@@ -98,6 +100,7 @@ public class InitDummyData {
             String formattedDate = randomDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 
             MemberEntity member = MemberEntity.builder()
+                    .kakaoId(Integer.toString(idx))
                     .email(Integer.toString(idx))
                     .birth(formattedDate)
                     .nickname(Integer.toString(idx))

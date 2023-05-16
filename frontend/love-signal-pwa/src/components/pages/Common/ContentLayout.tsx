d98da601af37cb75c2d21bd5170c2dd6524a7822
2sseul <iseul0626@gmail.com>
2023-05-16 13:56:49 +0900

    update: css edit

--- frontend/love-signal-pwa/src/components/pages/Common/ContentLayout.tsx
+++ frontend/love-signal-pwa/src/components/pages/Common/ContentLayout.tsx
@@ -9,6 +9,7 @@ import Modal_portal from "../../UI/Modal/Modal_portal";
 import AlarmModal from "../../UI/Modal/Alarm/AlarmModal";
 
 import style from "./styles/ContentLayout.module.scss";
+import { useState } from "react";
 
 let timeout: NodeJS.Timer;
 
@@ -16,11 +17,24 @@ const ContentLayout = () => {
   const [visible, setVisible] = useRecoilState<boolean>(alarmModal);
   const [animation, setAnimation] =
     useRecoilState<boolean>(alarmModalAnimation);
+  const [manualVisible, setManualVisible] = useState<boolean>(false);
+
+  const openManual = () => {
+    setAnimation(false);
+    clearTimeout(timeout);
+    setManualVisible(true);
+  };
 
   const openAlert = () => {
     setAnimation(false);
     clearTimeout(timeout);
     setVisible(true);
+  };
+
+  const closeManual = () => {
+    clearTimeout(timeout);
+    setAnimation(true);
+    timeout = setTimeout(() => setManualVisible(false), 300);
   };
 
   const closeAlert = () => {
@@ -31,13 +45,21 @@ const ContentLayout = () => {
 
   return (
     <>
-      <Header onClick={openAlert} />
+      <Header onClick={openAlert} openManual={openManual} />
       <div className="inner-main">
         {visible && (
           <Modal_portal>
             <div className={style.container}>
               <div className={style.background} onClick={closeAlert}></div>
               <AlarmModal closeModal={closeAlert}>알림창</AlarmModal>
+            </div>
+          </Modal_portal>
+        )}
+        {manualVisible && (
+          <Modal_portal>
+            <div className={style.container}>
+              <div className={style.background} onClick={closeManual}></div>
+              <AlarmModal closeModal={closeManual}>알림창</AlarmModal>
             </div>
           </Modal_portal>
         )}

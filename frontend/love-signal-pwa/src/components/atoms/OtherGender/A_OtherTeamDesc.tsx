78d30488afd8c48fed3a81b06c1f9ddcfebb44f1
2sseul <iseul0626@gmail.com>
2023-05-17 10:53:41 +0900

    fix: othergender animation:

--- frontend/love-signal-pwa/src/components/atoms/OtherGender/A_OtherTeamDesc.tsx
+++ frontend/love-signal-pwa/src/components/atoms/OtherGender/A_OtherTeamDesc.tsx
@@ -4,18 +4,12 @@ import { motion } from "framer-motion";
 
 const A_OtherTeamDesc = () => {
   return (
-    <motion.div
-      variants={contentVariants}
-      initial="hidden"
-      animate="visible"
-      // exit="exit"
-      className={style.description}
-    >
+    <div className={style.description}>
       러브하우스에 함께 입주하고 싶은 <span className="text-red">이성팀</span>
       에게
       <br />
       <span className="text-red">초대 메세지</span>를 보내세요
-    </motion.div>
+    </div>
   );
 };
 

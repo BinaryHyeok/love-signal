854ac2ee8de7781e2655e2fbbff5f929d62a287c
kwnoyng <kwnoyng@gmail.com>
2023-05-18 15:00:03 +0900

    delete folders

--- TeamService/src/main/java/kr/lovesignal/teamservice/service/MatchingService.java
+++
@@ -1,9 +0,0 @@
-package kr.lovesignal.teamservice.service;
-
-public interface MatchingService {
-
-    Long addTeamMatching(String strMemberUUID);
-
-    void cancelTeamMatching(String strMemberUUID);
-
-}

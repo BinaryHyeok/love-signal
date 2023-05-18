d8891ceb76f92ef6a2b08b5c90cb9412a97de8ab
kwnoyng <kwnoyng@gmail.com>
2023-05-18 10:48:53 +0900

    develop_auth

---
+++ AuthService/src/main/java/kr/lovesignal/authservice/exception/CustomException.java
@@ -0,0 +1,12 @@
+package kr.lovesignal.authservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException{
+
+    private final ErrorCode errorcode;
+
+}
